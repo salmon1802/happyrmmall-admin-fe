48d49636a6f04bff7f32cc237bc4248a6f173923
salmon1802 <757635525@qq.com>
2022-02-03 00:36:37 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ var config = {
     // path && publickPath
     output: {
         path        : __dirname + '/dist/',
-        publicPath  : WEBPACK_ENV === 'online' ? '//s.happymmall.com/mmall_admin_fe/dist/' : '/dist/',
+        publicPath  : WEBPACK_ENV === 'online' ? '//s.happyrmmall.cn/happyrmmall-admin-fe/dist/' : '/dist/',
         filename    : 'js/[name].js'
     },
     resolve: {
@@ -81,7 +81,7 @@ var config = {
         // html 加载
         new HtmlWebpackPlugin({
             filename        : 'view/index.html',
-            title           : 'MMall 后台管理系统',
+            title           : 'RMMall 后台管理系统',
             template        : './src/index.html',
             favicon         : './favicon.ico',
             inject          : true,
